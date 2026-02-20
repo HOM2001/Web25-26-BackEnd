2d687a4df45ae360eb4c3f82d58ed70476d77a50
54774 <54774@etu.he2b.be>
2026-02-20 20:52:10 +0100

    1) ajout de about et cgv via database corrigé

--- app/controller/static.php
+++ app/controller/static.php
@@ -5,7 +5,7 @@ function main_static()
     $static_name = $_GET['subpage'] ;
     $page_data = get_static_contents($static_name);
     if ($page_data) {
-        $title = $page_data['title_static'];
+        $title = '';
         $content = $page_data['content_static'];
     } else {
         $title = "404";
